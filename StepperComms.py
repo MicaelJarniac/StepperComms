555e16d85306adb1ef5b38a96594732bf3170033
Micael Jarniac <micael@jarniac.com>
2019-11-19 10:35:45 -0300

    Added comments and reorganized calculations to follow logical order

--- StepperComms.py
+++ StepperComms.py
@@ -1,54 +1,73 @@
+# Required imports
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.expanduser('~/projects/Python-Playground/Debug')))
 import serial
+sys.path.append(os.path.dirname(os.path.expanduser('~/projects/Python-Playground/Debug')))  # Update path accordingly
 from Debug.Debug import Debug
-debug = Debug(True, 3).prt
 
-RW_CMD              = 0x80
-TRANSFER_SIZE_MASK  = 0x3f
-BYTE_MASK           = 0xff
-RW_MASK             = 0x40
+# Declare debug
+debug = Debug(True, 3).prt  # Simplifies debugging messages
 
-READ                = 1
-WRITE               = 0
+# Message building blocks
+RW_CMD              = 0x80  # Validation check
+TRANSFER_SIZE_MASK  = 0x3f  # Masks bits used for transfer size
+BYTE_MASK           = 0xff  # Masks 1 byte
+RW_MASK             = 0x40  # Bit used for defining if 'read' or 'write' command type
 
-CMD_BUFFER_SIZE     = 61 + 2
+READ                = 1     # Command of type 'read'
+WRITE               = 0     #                 'write'
 
-OutCmdBuffer        = [None] * CMD_BUFFER_SIZE
-OutCmdBufferId      = 0
-CmdType             = 0
-CmdSize             = 0
-CmdAddr             = 0
+# Message size
+CMD_BUFFER_SIZE     = 1 + 1 + 61    # 1 byte (basic info & transfer size) + 1 byte (address) + 61 bytes (data)
 
-PORT                = "/dev/serial0"
-BAUD                = 9600
-TOUT                = 1
+# Message buffer and related
+OutCmdBuffer        = [None] * CMD_BUFFER_SIZE  # Initializes the buffer with given size
+OutCmdBufferId      = 0                         # Holds the current buffer position
 
+# Message parameters
+CmdType             = 0 # Command type ('read' or 'write')
+CmdSize             = 0 # Command size
+CmdAddr             = 0 # Command address
+
+# Serial configuration parameters
+PORT                = "/dev/serial0"    # Device
+BAUD                = 9600              # Baud rate
+TOUT                = 1                 # Timeout
+
+# Declare serial
 ser = serial.Serial(PORT, BAUD, timeout = TOUT)
 
+# Main loop
 while True:
+    # Clear serial in and out buffers
     ser.reset_input_buffer()
     ser.reset_output_buffer()
 
+    # Iterates through entire message buffer
     for i in range(CMD_BUFFER_SIZE):
         data    = 0
         CmdType = WRITE
         CmdSize = 1
         CmdAddr = 31
 
+        # Builds first byte
         if i == 0:
-            data |= RW_CMD & BYTE_MASK
-            data |= RW_MASK & (BYTE_MASK * CmdType)
-            data |= CmdSize & TRANSFER_SIZE_MASK
+            data |= RW_CMD & BYTE_MASK              # Validation check bit
+            data |= RW_MASK & (BYTE_MASK * CmdType) # Command type bit
+            data |= CmdSize & TRANSFER_SIZE_MASK    # Transfer size bits
+        # Builds second byte
         elif i == 1:
-            data |= CmdAddr & BYTE_MASK
+            data |= CmdAddr & BYTE_MASK # Address byte
+        # Builds third byte
         elif i == 2:
-            data |= 0x1 & BYTE_MASK
+            data |= 0x1 & BYTE_MASK # Placeholder
 
+        # Assigns built byte to its position on the message buffer
         OutCmdBuffer[i] = data
 
-    for i in range(CmdSize + 2):
-        ser.write(OutCmdBuffer[i] & BYTE_MASK)
+    # Iterates through 2 info bytes + command bytes
+    for i in range(2 + CmdSize):
+        ser.write(OutCmdBuffer[i] & BYTE_MASK)                  # Writes current message buffer position to the serial device
         debug("{1:02d} - {0:08b}".format(OutCmdBuffer[i], i))
+
     debug("\n")
